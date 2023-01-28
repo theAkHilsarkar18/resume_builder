8d2227932dfe0adfaf78779317f0859fd051cb80
theAkHilsarkar18 <akhilsodvadiya7@gmail.com>
2023-01-28 18:23:48 +0530

    2

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -8,7 +8,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -16,7 +15,7 @@ class _HomeScreenState extends State<HomeScreen> {
         body: Column(
           children: [
             Container(
-              height: 120,
+              height: 100,
               width: double.infinity,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.only(
@@ -77,52 +76,251 @@ class _HomeScreenState extends State<HomeScreen> {
                 ],
               ),
             ),
-            //
-            //
             SizedBox(
               height: 3,
             ),
-            boxUI("Personal Info",Icon(Icons.person,color: Colors.white,size: 20),),
-            boxUI("Contact Info",Icon(Icons.perm_phone_msg,color: Colors.white,size: 20),),
-            boxUI("Education",Icon(Icons.workspace_premium,color: Colors.white,size: 20),),
-
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget boxUI(String boxName,Icon iconName) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 8, right: 8, top: 5, bottom: 5),
-      child: Container(
-        alignment: Alignment.centerLeft,
-        padding: EdgeInsets.all(10),
-        height: 60,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-                color: Colors.grey,
-                spreadRadius: 1,
-                blurRadius: 1,
-                offset: Offset(0, 2)),
-          ],
-          gradient: LinearGradient(
-            colors: [
-              Colors.teal.shade900,
-              Colors.teal.shade700,
-            ],
-          ),
-        ),
-        child: Row(
-          children: [
-            iconName,
-            SizedBox(width: 10),
-            Text("$boxName",
-                style:
-                    TextStyle(color: Colors.white, letterSpacing: 1, fontSize: 15)),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(3),
+                      topRight: Radius.circular(3),
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10),
+                    ),
+                    child: ExpansionTile(
+                      initiallyExpanded: true,
+                      childrenPadding: EdgeInsets.all(10),
+                      backgroundColor: Colors.teal.shade800,
+                      collapsedBackgroundColor: Colors.teal.shade700,
+                      collapsedTextColor: Colors.white,
+                      collapsedIconColor: Colors.white,
+                      iconColor: Colors.white,
+                      leading:
+                          Icon(Icons.person, color: Colors.white, size: 20),
+                      title: Text(
+                        "Personal Info",
+                        style: TextStyle(
+                            color: Colors.white,
+                            letterSpacing: 2,
+                            fontSize: 18),
+                      ),
+                      children: [
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  height: 35,
+                                  width: double.infinity,
+                                  child: TextFormField(
+                                    keyboardType: TextInputType.name,
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                      letterSpacing: 1,
+                                    ),
+                                    decoration: InputDecoration(
+                                        labelText: "Name",
+                                        labelStyle: TextStyle(
+                                          letterSpacing: 1,
+                                          color: Colors.white,
+                                          fontSize: 16,
+                                        ),
+                                        border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(5),
+                                          borderSide: BorderSide(
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                        focusedBorder: OutlineInputBorder(
+                                            borderSide: BorderSide(
+                                                color: Colors.white, width: 2),
+                                            borderRadius:
+                                                BorderRadius.circular(10)),
+                                        enabledBorder: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(5),
+                                            borderSide: BorderSide(
+                                              width: 1.5,
+                                              color: Colors.white,
+                                            ))),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  height: 35,
+                                  width: double.infinity,
+                                  child: TextFormField(
+                                    keyboardType: TextInputType.name,
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                      letterSpacing: 1,
+                                    ),
+                                    decoration: InputDecoration(
+                                        labelText: "Surname",
+                                        labelStyle: TextStyle(
+                                          letterSpacing: 1,
+                                          color: Colors.white,
+                                          fontSize: 16,
+                                        ),
+                                        border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(5),
+                                          borderSide: BorderSide(
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                        focusedBorder: OutlineInputBorder(
+                                            borderSide: BorderSide(
+                                                color: Colors.white, width: 2),
+                                            borderRadius:
+                                                BorderRadius.circular(10)),
+                                        enabledBorder: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(5),
+                                            borderSide: BorderSide(
+                                              width: 1.5,
+                                              color: Colors.white,
+                                            ))),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            height: 50,
+                            width: double.infinity,
+                            child: TextFormField(
+                              keyboardType: TextInputType.name,
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                                letterSpacing: 1,
+                              ),
+                              decoration: InputDecoration(
+                                  labelText: "Address",
+                                  labelStyle: TextStyle(
+                                    letterSpacing: 1,
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                  ),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                    borderSide: BorderSide(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: Colors.white, width: 2),
+                                      borderRadius:
+                                          BorderRadius.circular(10)),
+                                  enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(5),
+                                      borderSide: BorderSide(
+                                        width: 1.5,
+                                        color: Colors.white,
+                                      ))),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            height: 35,
+                            width: double.infinity,
+                            child: TextFormField(
+                              keyboardType: TextInputType.emailAddress,
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                                letterSpacing: 1,
+                              ),
+                              decoration: InputDecoration(
+                                  labelText: "Email",
+                                  labelStyle: TextStyle(
+                                    letterSpacing: 1,
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                  ),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                    borderSide: BorderSide(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: Colors.white, width: 2),
+                                      borderRadius:
+                                          BorderRadius.circular(10)),
+                                  enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(5),
+                                      borderSide: BorderSide(
+                                        width: 1.5,
+                                        color: Colors.white,
+                                      ))),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            height: 35,
+                            width: double.infinity,
+                            child: TextFormField(
+                              keyboardType: TextInputType.phone,
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                                letterSpacing: 1,
+                              ),
+                              decoration: InputDecoration(
+                                  labelText: "Phone",
+                                  labelStyle: TextStyle(
+                                    letterSpacing: 1,
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                  ),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(5),
+                                    borderSide: BorderSide(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: Colors.white, width: 2),
+                                      borderRadius:
+                                          BorderRadius.circular(10)),
+                                  enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(5),
+                                      borderSide: BorderSide(
+                                        width: 1.5,
+                                        color: Colors.white,
+                                      ))),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
